3f2dac893d67d1cfdd79300af7cbc93f423fd313
DeltaByte <thedeltabyte@gmail.com>
2020-02-14 16:47:36 +0000

    add basic navbar layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,56 @@
+<template>
+  <mdb-navbar color="secondary-color-dark" dark>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav>
+        <mdb-nav-item href="#" active>Home</mdb-nav-item>
+      </mdb-navbar-nav>
+      <mdb-form-inline class="ml-auto">
+        <mdbIcon icon="search" />
+        <mdb-input
+          type="text"
+          class="text-white"
+          placeholder="Search..."
+          aria-label="Search..."
+          label
+          nav-input
+          disabled
+          title="Not available yet"
+        />
+      </mdb-form-inline>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
+</template>
+
+<script>
+import {
+  mdbNavbar,
+  // mdbNavbarBrand,
+  mdbNavbarToggler,
+  mdbNavbarNav,
+  mdbNavItem,
+  // mdbDropdown,
+  // mdbDropdownMenu,
+  // mdbDropdownToggle,
+  mdbInput,
+  // mdbDropdownItem,
+  mdbIcon,
+  mdbFormInline
+} from 'mdbvue'
+export default {
+  name: 'Navbar',
+  components: {
+    mdbNavbar,
+    // mdbNavbarBrand,
+    mdbNavbarToggler,
+    mdbNavbarNav,
+    mdbNavItem,
+    // mdbDropdown,
+    // mdbDropdownMenu,
+    // mdbDropdownToggle,
+    // mdbDropdownItem,
+    mdbInput,
+    mdbIcon,
+    mdbFormInline
+  }
+}
+</script>
