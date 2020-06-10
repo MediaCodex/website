0eef7bfd036a4e0824116a472fe27ae38d4a97b7
DeltaByte <thedeltabyte@gmail.com>
2020-06-10 17:25:53 +0100

    add link to WIP sections

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <p>{{ $store.state.auth.authenticated ? 'yay' : 'nay' }}</p>
-  </div>
+  <v-container fluid>
+    <p style="text-align: center;">
+      authenticated: {{ $store.state.auth.authenticated ? 'yay' : 'nay' }}
+    </p>
+    <h4>WIP sections:</h4>
+    <ul>
+      <li><nuxt-link to="/anime">Anime</nuxt-link></li>
+    </ul>
+  </v-container>
 </template>
