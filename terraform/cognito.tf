31a56e6c4b3812965561d4806450cc96812b17bc
DeltaByte <thedeltabyte@gmail.com>
2020-07-19 14:16:50 +0100

    export cognito client

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -2,3 +2,7 @@ resource "aws_cognito_user_pool_client" "default" {
   name         = "client"
   user_pool_id = data.terraform_remote_state.core.outputs.cognito_pool
 }
+
+output "cognito_client" {
+  value = aws_cognito_user_pool_client.default.id
+}
