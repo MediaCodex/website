d8ba8f33fbc23e1f4987b9cd921aaa047015a787
DeltaByte <thedeltabyte@gmail.com>
2020-07-22 19:51:59 +0100

    set firebase env via terraform

--- terraform/nuxt.tf
+++ terraform/nuxt.tf
@@ -1,12 +1,22 @@
+locals {
+  firebase_env = lookup(var.firebase_environments, local.environment)
+}
+
+variable "firebase_environments" {
+  type = map
+  default = {
+    dev  = "development"
+    prod = "production"
+  }
+}
+
 /*
  * These are outputs used for the CI to build the vuejs app
  */
 output "nuxt" {
   description = "Env config for nuxt build in CI"
   value = templatefile("${path.module}/nuxt.tpl", { env = {
-    DOMAIN_NAME            = local.domain,
-    AMPLIFY_AUTH_REGION    = "eu-central-1",
-    AMPLIFY_AUTH_POOL_ID   = data.terraform_remote_state.core.outputs.cognito_pool,
-    AMPLIFY_AUTH_CLIENT_ID = aws_cognito_user_pool_client.default.id
+    DOMAIN_NAME = local.domain,
+    FIRE_ENV    = local.firebase_env
   } })
 }
