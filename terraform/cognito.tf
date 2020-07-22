e08cb0df177b399434c264fbf21bc4f480b23ebb
DeltaByte <thedeltabyte@gmail.com>
2020-07-22 19:52:26 +0100

    remove cognito

--- terraform/cognito.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_cognito_user_pool_client" "default" {
-  name         = "client"
-  user_pool_id = data.terraform_remote_state.core.outputs.cognito_pool
-}
-
-output "cognito_client" {
-  value = aws_cognito_user_pool_client.default.id
-}
