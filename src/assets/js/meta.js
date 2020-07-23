50d1a93d31b0d96cb5c8916c53acfcaab5c42a4d
DeltaByte <thedeltabyte@gmail.com>
2020-07-23 01:41:08 +0100

    disable crawling if not prod

--- src/assets/js/meta.js
+++ src/assets/js/meta.js
@@ -1,1 +1,6 @@
-export default [{ name: 'google', content: 'notranslate' }]
+const isProd = process.env.FIRE_ENV === 'production'
+
+export default [
+  { name: 'google', content: 'notranslate' },
+  ...(!isProd && { name: 'robots', content: 'noindex' })
+]
