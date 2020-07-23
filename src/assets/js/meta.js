f9bdba2d5c1921eeff2bf01504123a522368bf28
DeltaByte <thedeltabyte@gmail.com>
2020-07-23 22:19:57 +0100

    refactor meta

--- src/assets/js/meta.js
+++ src/assets/js/meta.js
@@ -1,6 +1,31 @@
-const isProd = process.env.FIRE_ENV === 'production'
+/**
+ * Default meta tags
+ *
+ * @constant {Array<Object>} defaultMeta
+ */
+const defaultMeta = [{ name: 'google', content: 'notranslate' }]
 
-export default [
-  { name: 'google', content: 'notranslate' },
-  ...(!isProd && { name: 'robots', content: 'noindex' })
-]
+/**
+ * Meta tags for development environments
+ *
+ * @constant {Array<Object>} devMeta
+ */
+const devMeta = [{ name: 'robots', content: 'noindex' }]
+
+/**
+ * Combine the various metas based on env
+ *
+ * @returns {Array<Object>}
+ */
+const meta = () => {
+  let meta = defaultMeta
+
+  // non-prod meta
+  if (process.env.FIRE_ENV !== 'production') {
+    meta = meta.concat(devMeta)
+  }
+
+  return meta
+}
+
+export default meta()
